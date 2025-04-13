e410cde112080e197cefab76330d1c1c11ef78b3
Dimitris Kopsidas <jimkopsidas@hotmail.com>
2025-04-13 18:03:08 +0300

    minigame testing

---
+++ js/minigame.js
@@ -0,0 +1,22 @@
+import {Wheel} from 'https://cdn.jsdelivr.net/npm/spin-wheel@5.0.2/dist/spin-wheel-esm.js';
+window.onload = () => {
+
+    const props = {
+      items: [
+        {
+          label: 'one',
+        },
+        {
+          label: 'two',
+        },
+        {
+          label: 'three',
+        },
+      ],
+    };
+  
+    const container = document.querySelector('.wheel-wrapper');
+  
+    window.wheel = new Wheel(container, props);
+  
+  };
